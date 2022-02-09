9dd2f3563049c7d833d157b1d18090ff12a56a5a
Gabriel Tomsic <tomsicgabriel@gmail.com>
2022-02-09 11:26:03 -0700

    Problem 8 update done

---
+++ check.js
@@ -0,0 +1,8 @@
+let score = 0;
+let passingScore = 7;
+//CODE HERE
+while (passingScore > 0) {
+  score = score + 1;
+  passingScore = passingScore - 1;
+  console.log(`Score ${score}`);
+}
--- lab.js
+++ lab.js
@@ -120,7 +120,7 @@ let passingScore = 7;
 */
 
 //CODE HERE
-while (passingScore !== 0) {
+while (passingScore > 0) {
   score = score + 1;
   passingScore = passingScore - 1;
   console.log(`Score ${score}`);
