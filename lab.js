fc5229ffdb1be9b53e80c5faf9ee28d35dbc4a90
Gabriel Tomsic <tomsicgabriel@gmail.com>
2022-02-09 11:19:11 -0700

    Problem 10 done changing changeMyMin either true or false

--- lab.js
+++ lab.js
@@ -144,6 +144,12 @@ if (changeMyMind) {
 ////////// PROBLEM 10 //////////
 // Try to change the value of changeMyMind (irrespective of whether it is true or false) and then console.log it's new status. It should log true. Hint: use the not operator.
 
+if (changeMyMind === true) {
+  console.log(`changeMyMind = ${!changeMyMind} now`);
+} else if (changeMyMind === false) {
+  console.log(`changeMyMind = ${!changeMyMind} now`);
+}
+
 //CODE HERE
 
 ////////// ADVANCED PROBLEMS //////////
